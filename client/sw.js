6623f3c4e3187a23e0785001b82e56f7541f3049
Nickname <55950404+Nicknaem@users.noreply.github.com>
2023-02-01 03:25:24 +0400

    pwa update

---
+++ client/sw.js
@@ -0,0 +1,21 @@
+const cacheName = "cache3"; // Change value to force update
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll([
+        '/',
+        'index.html',
+        'styles/main.css',
+        'styles/mywind.css',
+        'assets/logo.png'
+    ])),
+  );
+});
+ 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
