778ee2603399a8a634a6a358469115c6c61fe418
Nickname <55950404+Nicknaem@users.noreply.github.com>
2023-05-09 07:57:38 +0400

    pwa update

---
+++ client/sw old.js
@@ -0,0 +1,44 @@
+const cacheName = "cache3"; // Change value to force update
+
+self.addEventListener('install', event => {
+    // Kick out the old service worker
+	self.skipWaiting();
+
+    event.waitUntil(
+      caches.open(cacheName).then(cache => cache.addAll([
+        '/',
+        'index.html',
+        'styles/main.css',
+        'styles/mywind.css',
+        'assets/logo.png'
+      ])),
+    );
+  });
+
+  self.addEventListener("activate", event => {
+	// Delete any non-current cache
+	event.waitUntil(
+		caches.keys().then(keys => {
+			Promise.all(
+				keys.map(key => {
+					if (![cacheName].includes(key)) {
+						return caches.delete(key);
+					}
+				})
+			)
+		})
+	);
+});
+
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open(cacheName).then(cache => {
+			return cache.match(event.request).then(response => {
+				return response || fetch(event.request).then(networkResponse => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+			})
+		})
+	);
+});
